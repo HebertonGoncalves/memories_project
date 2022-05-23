5d669911623af46b93b95e0bb480ddde3069401d
Heberton Torres <71787106+HebertonGoncalves@users.noreply.github.com>
2022-05-23 07:13:34 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://adm_mern_1:42LmDJjyFyfwKr9z@cluster0.0w74v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://adm_mern_1:2LmDJjyFyfwKr9z@cluster0.0w74v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
